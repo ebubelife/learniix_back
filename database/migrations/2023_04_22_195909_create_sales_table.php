658ec605611f51f39b9c2e9a8a08ffe25c6f767b
Ebube Life <ebubeemeka19@gmail.com>
2023-04-22 21:09:06 +0100

    update

---
+++ database/migrations/2023_04_22_195909_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('vendor_id')->nullable();
+            $table->string('affiliate_id')->nullable();
+            $table->string('product_id')->nullable();
+            $table->string('product_price')->nullable();
+            $table->string('commission')->nullable();
+            $table->string('tx_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
