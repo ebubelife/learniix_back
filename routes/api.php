b7e51ab3420a97097b56d8d8c1f13fb8641d9128
Ebube Life <ebubeemeka19@gmail.com>
2023-04-23 20:49:07 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -78,7 +78,7 @@ Route::controller(ProductsController::class)->group(function(){
    Route::get('view/product/{id}', function ($id) {
     $user = Products::find($id);
 
-    $user->image = asset('https://zenithstake.syncight.com/storage/images/product_images/' . $user->image)
+    $user->image = asset('https://zenithstake.syncight.com/storage/images/product_images/' . $user->image);
 
     return response()->json($user);
 });
