e7b7af4007f59d4c2b04e636105afade65eb25d2
Ebube Life <ebubeemeka19@gmail.com>
2023-04-29 13:11:33 +0100

    update

--- resources/views/test_email.blade.php
+++ resources/views/test_email.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<div style="padding:10px; border:1px solid #cccccc; text-align:center; font-size:20px;">
-	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" />
+	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="100" width="300" />
 
 
 	<h1>Hi  {{ $firstName }}, </h1>
