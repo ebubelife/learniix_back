32eee476adc19cf3ab3999f09d80ab98759f5f2d
Ebube Life <ebubeemeka19@gmail.com>
2023-04-29 13:33:21 +0100

    update

--- resources/views/recover_email.blade.php
+++ resources/views/recover_email.blade.php
@@ -8,7 +8,7 @@
 	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="80" width="270" />
 
 
-	<h1>Hi  {{ $firstName }}, </h1>
+	<h2>Hi  {{ $firstName }}, </h2>
 	<p>We have just received a request to reset the password of your account on <b>2023-04-03 08:31:35 PM.</b></p>
 
 	<p>Requested From-  IP Address: <b>192.210.85.220</b> using <b>Firefox</b> on <b>Windows 10</b></p>
@@ -18,7 +18,7 @@
 
 	<p style="margin-top:20px; color:red">If you're not the one who made this request and you're not aware of it, disregard this message.</p>
 
-	<p style="margin-top:20px; color:#cccccc">© zenithstake.com 2023</p>
+	<p style="margin-top:20px; color:#dcdcdc">© zenithstake.com 2023</p>
 
 </div>
 </body>
