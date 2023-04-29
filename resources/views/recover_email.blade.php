a25e4557531c8e577fb9304dba9686734d71aa6b
Ebube Life <ebubeemeka19@gmail.com>
2023-04-29 17:53:47 +0100

    update

--- resources/views/recover_email.blade.php
+++ resources/views/recover_email.blade.php
@@ -8,10 +8,10 @@
 	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="80" width="270" />
 
 
-	<h3>Hi  {{ $firstName }}, </h3>
-	<p style="margin-top:20px; color:red">You requested a password reset, please copy your account recovery code below</p>
+	<p style="color:#696969; text-size:21px;">Hi  {{ $firstName }}, </h3>
+	<p style="margin-top:20px; color:#696969;">You requested a password reset, please copy your account recovery code below</p>
 
-		<h3 style="color:#5B2C6F">{{ $emailCode }}</h3>
+		<h3 style="color:#5B2C6F;">{{ $emailCode }}</h3>
 
 	<p style="margin-top:20px; color:red">If you're not the one who made this request and you're not aware of it, disregard this message.</p>
 
