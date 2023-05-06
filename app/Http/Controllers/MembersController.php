1ffe70165aaba555ec0d0567813d5347a704b359
Ebube Life <ebubeemeka19@gmail.com>
2023-05-06 16:54:51 +0100

    update

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -353,7 +353,7 @@ public function checkPhoneExists($email)
             $user->save();
 
             if(Mail::to($request->email)->send(new RecoverAccountMail(  $emailCode,$firstName  ))){
-                return response()->json(['message'=>'We\'ve sent you an email to help you recover your password'],200);
+                return response()->json(['message'=>'We\'ve sent you an email to help you recover your password'],100);
 
             }
         }
