2c7a37af106656694037d7d66fe0e1ffc7ca899d
Ebube Life <ebubeemeka19@gmail.com>
2023-05-15 20:47:39 +0100

    ...

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -172,7 +172,7 @@ class SalesController extends Controller
         $to = $validated["to_date"];
 
       //  if($validated["selected_products"] == ''){
-            $sales_by_user = Sales::where('affiliate_id',$validated["affiliate_id"])->
+            $sales_by_user = Sales::where('affiliate_id',$validated["affiliate_id"])
             ->where('created_at', '>=', Carbon::parse($from))
             ->where('created_at', '<=', Carbon::parse($to))
             ->get();
