fb872253be9d90dc072d5ffeca651ea52aef1887
Ebube Life <ebubeemeka19@gmail.com>
2023-05-16 11:15:23 +0100

    update

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -140,7 +140,7 @@ class SalesController extends Controller
     {
         $sales = Sales::join('products', 'sales.product_id', '=', 'products.id')
         ->join('members', 'products.vendor_id', '=', 'members.id')
-        ->select('sales.*', 'products.productName as product_name', 'products.price', 'members.firstName as affiliate_name')
+        ->select('sales.*', 'products.productName as product_name', 'products.productPrice', 'members.firstName as affiliate_name')
         ->get();
 
     return response()->json($sales);
