5669cabe78f525303b50804474ba6105ef9843af
Ebube Life <ebubeemeka19@gmail.com>
2023-05-20 11:50:14 +0100

    ...

--- database/migrations/2023_05_20_104702_add_to_banks.php
+++ database/migrations/2023_05_20_104702_add_to_banks.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Banks extends Migration
+class AddToBanks extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class Banks extends Migration
      */
     public function up()
     {
-        //
-
-        Schema::create('banks', function (Blueprint $table) {
-            $table->id();
-            $table->string('bank');
-            $table->string('code');
-            $table->timestamps()->nullable();
+        Schema::table('banks', function (Blueprint $table) {
+            //
+            $table->string('code')->nullabele();
         });
     }
 
@@ -30,6 +26,8 @@ class Banks extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('banks', function (Blueprint $table) {
+            //
+        });
     }
 }
