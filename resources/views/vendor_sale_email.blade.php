3921ba53d758da7e4f415f4151dd00b890ecd9f6
Ebube Life <ebubeemeka19@gmail.com>
2023-05-25 02:35:51 +0100

    ...

--- resources/views/vendor_sale_email.blade.php
+++ resources/views/vendor_sale_email.blade.php
@@ -8,14 +8,14 @@
 	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="80" width="270" />
 
 
-	<p style="color:#696969; font-size:21px;">Hi  {{ firstName }} , </h3>
+	<p style="color:#696969; font-size:21px;">Hi  {{ $firstName }} , </h3>
 	<p style="margin-top:20px; color:#696969;">You just made a successful sale for the product below</p>
 
     <p style="margin-top:15px; color:#000000; font-weight:bold">SMAC COURSE</p>
 
-    <p style="margin-top:12px; color:#696969;">Price :  N {{ productPrice }}</p>
+    <p style="margin-top:12px; color:#696969;">Price :  N {{ $productPrice }}</p>
 
-    <p style="margin-top:12px; color:#0000000;">Commission :  N  {{ commission }}</p>
+    <p style="margin-top:12px; color:#0000000;">Commission :  N  {{ $commission }}</p>
 
 
 		
