8c1d9354463e0d4b9de4047de2c0b699d4ad5498
Ebube Life <ebubeemeka19@gmail.com>
2023-05-26 12:42:54 +0100

    ....

--- app/Http/Controllers/VendorsController.php
+++ app/Http/Controllers/VendorsController.php
@@ -45,8 +45,8 @@ class VendorsController extends Controller
             $validated = $request->validate([
                 'businessName' => 'required|string',
                 'bio' => 'required|string',
-                'id'=>  'required',
-                'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:10280',
+                'id'=>  '4',
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif',
                
             ],
            
