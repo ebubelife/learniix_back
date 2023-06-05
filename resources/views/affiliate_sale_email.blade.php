fc3bf9545699ae3abb32a364e76e66962d271b33
Ebube Life <ebubeemeka19@gmail.com>
2023-06-05 17:08:46 +0100

    ...

--- resources/views/affiliate_sale_email.blade.php
+++ resources/views/affiliate_sale_email.blade.php
@@ -6,7 +6,7 @@
 <body>
 	<div style="padding:10px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;  border-radius:20px; font-size:16px;">
 	
-	<div style="width:100%;padding-top:10px; padding-bottom:10px; background-color:#cdcdcd; text-align:center">
+	<div style="width:100%;padding-top:10px; padding-bottom:10px; background-color:#E5E4E2; text-align:center">
 	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="80" width="270" style="display:inline-block" />
 
 </div>
@@ -23,8 +23,10 @@
 
 
 		
+	<div style="width:100%;padding-top:10px; padding-bottom:10px; background-color:#E5E4E2; text-align:center">
+	<p style="margin-top:20px; color:#000000; display:inline-block;"> zenithstake.com © 2023</p>
+</div>
 	
-	<p style="margin-top:20px; color:#B2BEB5">© zenithstake.com 2023</p>
 
 </div>
 </body>
