31f6733e954e38319fb384655d809d09fa80840f
Ebube Life <ebubeemeka19@gmail.com>
2023-06-05 16:54:38 +0100

    ....

--- resources/views/affiliate_sale_email.blade.php
+++ resources/views/affiliate_sale_email.blade.php
@@ -5,22 +5,25 @@
 </head>
 <body>
 	<div style="padding:10px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;  border-radius:20px; font-size:16px;">
-	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="80" width="270" />
+	
+	<div style="width:100%;padding-top:10px; padding-bottom:10px; background-color:#cdcdcd; text-center">
+	<img src="https://www.zenithstake.com/images/logo.png" alt="logo" height="80" width="270" style="display:inline-block" />
+
+</div>
 
 
-	<p style="color:#696969; font-size:21px;">Hi  {{ $firstName }} , </h3>
+	<p style="color:#696969; font-size:20px;font-weight:bold; ">Hi  {{ $firstName }} , </h3>
 	<p style="margin-top:20px; color:#696969;">You just made a successful sale for the product below</p>
 
     <p style="margin-top:15px; color:#000000; font-weight:bold">SMAC COURSE</p>
 
     <p style="margin-top:12px; color:#696969;">Price :  ₦{{ $productPrice }}</p>
 
-    <p style="margin-top:12px; color:#0000000;">Commission :  ₦{{ $commission }}</p>
+    <p style="margin-top:12px; color:#0000000;">You have just earned  ₦{{ $commission }}  on this sale</p>
 
 
 		
-	<p style="margin-top:20px; color:red">If you are receiving this email in error, please disregard it</p>
-
+	
 	<p style="margin-top:20px; color:#B2BEB5">© zenithstake.com 2023</p>
 
 </div>
