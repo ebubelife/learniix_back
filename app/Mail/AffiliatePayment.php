f0bfbe627e521492658615b08e07364ea140779a
Ebube Life <ebubeemeka19@gmail.com>
2023-06-06 18:23:55 +0100

    ...

---
+++ app/Mail/AffiliatePayment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class AffiliatePayment extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+     public $affiliate_name;
+     public $amount;
+    public function __construct()
+    {
+        //
+        $this->affiliate_name; = $affiliate_name
+        $this->amount= $amount;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $message = 'This is an example email sent from Laravel.';
+        return $this->view('affiliate_payment_email', ['message' => $message])
+                    ->with(['name'=> $this->affiliate_name, 'amount'=> $this->amount])
+                   
+                    ->from('zenithstake@gmail.com')
+                    ->subject('ZenithStake - Welcome!🥳🥳 To ZenithStake!🥳');
+    }
+}
