2b2622607d49046e37162323c4d0ce27015af855
Ebube Life <ebubeemeka19@gmail.com>
2023-06-06 19:02:35 +0100

    ...

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -111,7 +111,7 @@ class TransactionsController extends Controller
 
            if($res["status"]=="success"){
 
-            Mail::to($unpaid_user->email)->send(new AffiliatePayment( "100","Ebube"."Emeka"));
+            Mail::to($unpaid_user->email)->send(new AffiliatePayment( $unpaid_user->unpaid_balance,$unpaid_user->firstName." ".$unpaid_user->lastName));
             $single_tx_result = array("user"=>$unpaid_user->id,"result"=>$res); 
 
             array_push($all_tx_result, $single_tx_result);
