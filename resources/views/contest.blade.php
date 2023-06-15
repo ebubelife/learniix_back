a885ac671b3e19c019e066ebd83cfdd7515ca689
Ebube Life <ebubeemeka19@gmail.com>
2023-06-15 14:29:00 +0100

    ...

--- resources/views/contest.blade.php
+++ resources/views/contest.blade.php
@@ -19,53 +19,53 @@
 </div>
 
 
-	<p style="color:#000000; font-size:20px;font-weight:bold; margin-top:20px; ">In the last few weeks, the Sales Machine Accelerator Course has transformed a lot of lives financially and knowledge-wise. </p>
+	<p style="color:#000000; font-size:20px; margin-top:20px; ">In the last few weeks, the Sales Machine Accelerator Course has transformed a lot of lives financially and knowledge-wise. </p>
 
 
 
-    <p style="color:#000000; font-size:20px;font-weight:bold; margin-top:20px; ">And this time, we are raising the bar, and we want you to join the league of the big boys. </p>
+    <p style="color:#000000; font-size:20px; margin-top:20px; ">And this time, we are raising the bar, and we want you to join the league of the big boys. </p>
 
-    <p style="color:#000000; font-size:20px;font-weight:bold; margin-top:20px; ">So, here comes the opportunity you've been waiting for to earn big and better your life. </p>
+    <p style="color:#000000; font-size:20px; margin-top:20px; ">So, here comes the opportunity you've been waiting for to earn big and better your life. </p>
 
-    <p style="color:#000000; font-size:20px;font-weight:bold; margin-top:20px; ">So, here comes the opportunity you've been waiting for to earn big and better your life. </p>
+    <p style="color:#000000; font-size:20px; margin-top:20px; ">So, here comes the opportunity you've been waiting for to earn big and better your life. </p>
 
 
-    <p style="color:#000000; font-size:20px;font-weight:bold; margin-top:20px; "> The SMAC challenge 2.0 is a 75-day challenge  with 2 categories mapped out to help you make more money and gain access to life changing opportunities.</p>
+    <p style="color:#000000; font-size:20px; margin-top:20px; "> The SMAC challenge 2.0 is a 75-day challenge  with 2 categories mapped out to help you make more money and gain access to life changing opportunities.</p>
 
 
     <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">CATEGORY 1 </p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:10px;">From 15th of June to 15th of July </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px;margin-top:10px;">From 15th of June to 15th of July </p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">10 sales of SMAC  - Win for yourself 15,000 naira. </p>
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">20 sales of SMAC  - Win for yourself 30,000 naira. </p>
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">30 sales of SMAC -  win for yourself 45,000 naira. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">10 sales of SMAC  - Win for yourself 15,000 naira. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">20 sales of SMAC  - Win for yourself 30,000 naira. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">30 sales of SMAC -  win for yourself 45,000 naira. </p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">50 sales of SMAC - Win for yourself 75,000 naira.</p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">50 sales of SMAC - Win for yourself 75,000 naira.</p>
 	
 
     <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">CATEGORY 2 </p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:10px;">From 15th of June to 31st of August </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:10px;">From 15th of June to 31st of August </p>
 
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">100 sales of SMAC  - Win for yourself 150,000 naira. </p>
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">150 sales of SMAC - Win for yourself a ring light, sound microphone, and #200,000 cash. </p>
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">200 sales of SMAC  - win for yourself a brand new smartphone, Redmi Note 128 8GB RAM, 256GB ROM, and #200,000 cash. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px;margin-top:20px;">100 sales of SMAC  - Win for yourself 150,000 naira. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">150 sales of SMAC - Win for yourself a ring light, sound microphone, and #200,000 cash. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">200 sales of SMAC  - win for yourself a brand new smartphone, Redmi Note 128 8GB RAM, 256GB ROM, and #200,000 cash. </p>
 
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">300 sales of SMAC - Win for yourself a brand new Laptop HP Notebook 15 core -i5 - touch 12GB RAM, 1TB, and #200,000 cash. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">300 sales of SMAC - Win for yourself a brand new Laptop HP Notebook 15 core -i5 - touch 12GB RAM, 1TB, and #200,000 cash. </p>
 
 
     <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">NOTE:</p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">Any or all prizes in these two categories must be won within the designated period. </p>
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">So, it's time to set that sales goal and crush it big time.💪 </p>
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">See yourself as a winner, not just in words but in actions. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">Any or all prizes in these two categories must be won within the designated period. </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">So, it's time to set that sales goal and crush it big time.💪 </p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">See yourself as a winner, not just in words but in actions. </p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">We are rooting for you, See you on the winning side.💃</p>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">We are rooting for you, See you on the winning side.💃</p>
 
-    <p style="color:#000000; font-weight:bold; font-size:20px;font-weight:bold; margin-top:20px;">Kind regards,<br>
+    <p style="color:#000000; font-weight:bold; font-size:20px; margin-top:20px;">Kind regards,<br>
 Zenithstake's team.✍🏼</p>
 
 
