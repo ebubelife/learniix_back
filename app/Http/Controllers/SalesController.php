fa40c99dd0ea5c767688a8203ba15284b8f9253d
Ebube Life <ebubeemeka19@gmail.com>
2023-06-29 23:15:29 +0100

    ...

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -173,13 +173,15 @@ class SalesController extends Controller
           if( $affiliate->save()){
             if($validated["product_id"] == "1"){
 
-                Mail::to($validated["customer_email"])->send(new FinishReg($validated["customer_name"],$sale->id))
+                Mail::to($validated["customer_email"])->send(new CourseAccess($validated["customer_name"], $product->ProductTYLink ))
+               
+
+              
             }
 
             else{
 
-                Mail::to($validated["customer_email"])->send(new CourseAccess($validated["customer_name"], $product->ProductTYLink ))
-               
+                Mail::to($validated["customer_email"])->send(new FinishReg($validated["customer_name"],$sale->id))
 
             }
 
