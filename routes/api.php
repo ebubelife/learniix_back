a90bd48f399060a28f9763ef3ddb2a375410c98c
ebubelife <ebubeemeka19@gmail.com>
2023-12-02 19:41:34 +0100

    ...

--- routes/api.php
+++ routes/api.php
@@ -889,7 +889,7 @@ Route::get('sales/today/duplicates', function () {
     $startDate = '2023-11-27'; // Replace with your start date
     $endDate = '2023-12-02';   // Replace with your end date
 
-    $sales = Sales::where("affiliate_id", "8YGrs3")
+    $sales = Sales::where("affiliate_id", "nDRfut")
         ->whereBetween('created_at', [$startDate, $endDate])
         ->get();
 
