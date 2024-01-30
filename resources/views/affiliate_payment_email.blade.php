668efce19f91ebff71ffcc25c63e973832bde6ac
ebubelife <ebubeemeka19@gmail.com>
2024-01-30 18:41:29 +0100

    ...

--- resources/views/affiliate_payment_email.blade.php
+++ resources/views/affiliate_payment_email.blade.php
@@ -7,25 +7,24 @@
 <body>
 	<div style="padding:10px; box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;  border-radius:20px; font-size:16px;">
 	
-	<div style="width:100%;padding-top:10px; padding-bottom:10px;  text-align:center">	<img src="https://www.learniix.com/images/logo.jpeg" alt="logo" height="80" width="270" style="display:inline-block" />
+	<div style="width:100%;padding-top:10px; padding-bottom:10px;  text-align:center">	<img src="https://learniix.com/images/home/learniix_logo.jpeg" alt="logo" height="80" width="270" style="display:inline-block" />
 
 </div>
 
 
 	<p style="color:#000000; font-size:20px;font-weight:bold; margin-top:20px; ">Dear {{ $affiliate_name }}, </p>
-	<p style="margin-top:20px; color:#696969;margin-top:20px;">You have received a payment of <span style="color:#000000; font-weight:bold">${{ $amount }}</span> from Zenithstake </p>
+	<p style="margin-top:20px; color:#696969;margin-top:20px;">You have received a payment of <span style="color:#000000; font-weight:bold">${{ $amount }}</span> from Learniix </p>
 
   
 
-	<p style="margin-top:20px; color:#696969;margin-top:20px;">Keep making more sales and credit alert is sure!..</p>
+	<p style="margin-top:20px; color:#696969;margin-top:20px;">Keep up with the efforts and keep raking in the cash!..</p>
 
-	<p style="margin-top:20px; color:#696969;margin-top:20px;">Note: This email is to inform you that your payment is being processed and doesn't automatically confirm bank credit alert.</p>
+	<p style="margin-top:20px; color:#696969;margin-top:20px;">Note: This email is to inform you that your payment is being processed and might not reflect in your bank accounts immediately.</p>
 	
 	
-	<p style="margin-top:20px; color:#696969;margin-top:20px;">Payment status: <span style="color:#4CBB17">Credited!</span></p>
-
+	
 	<p style="margin-top:20px; color:#696969;margin-top:20px;">
-	Credit alert will take 0-48hrs depending on your bank network
+	Please allow 0-48 hours for the cash to be deposited in your bank account.
 </p>
 
 	<div style="width:100%;padding-top:10px; padding-bottom:10px;  text-align:center">	<p style="margin-top:20px; color:#32cd32; display:inline-block;"> learniix.com © 2024</p>
