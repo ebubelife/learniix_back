e629f9bce392829a5c796cc7fbea2a986ccd204a
ebubelife <ebubeemeka19@gmail.com>
2024-08-12 10:23:19 +0100

    update

---
+++ database/migrations/2024_08_12_092204_modify_members_table1.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyMembersTable1 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('members', function (Blueprint $table) {
+            $table->timestamp('next_withdrawal_amount')->nullable()->default("0");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
